5cf91093476ca3f33fb838195c7e96f39d6966e9
Sergey Zubarev <info@js-labs.org>
2013-08-28 21:29:14 +0400

    Redesigned test.

---
+++ tests/src/org/jsl/tests/thread_pool/Test.java
@@ -0,0 +1,36 @@
+/*
+ * JS-Collider framework.
+ * Copyright (C) 2013 Sergey Zubarev
+ * info@js-labs.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jsl.tests.thread_pool;
+
+public abstract class Test
+{
+    protected final int m_totalEvents;
+    protected final int m_producers;
+
+    protected Test( int totalEvents, int producers )
+    {
+        assert( (totalEvents % producers) == 0 );
+        m_totalEvents = totalEvents;
+        m_producers = producers;
+    }
+
+    public abstract String getName();
+    public abstract long runTest();
+}
