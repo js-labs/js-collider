b55a15a3f51751de1930e143c39e0090424651c9
Sergey Zubarev <info@js-labs.org>
2013-09-07 23:22:12 +0400

    Cosmetic fix.

--- tests/src/org/jsl/tests/session_latency/Main.java
+++ tests/src/org/jsl/tests/session_latency/Main.java
@@ -18,29 +18,99 @@
  */
 
 package org.jsl.tests.session_latency;
+
+import org.jsl.collider.Acceptor;
+import org.jsl.collider.Collider;
+import org.jsl.collider.Session;
+
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main
 {
-    public static void main( String args[] )
+    private final int m_sessions;
+    private final int m_messages;
+    private final int m_messageLength;
+    private final AtomicInteger m_sessionsDone;
+    private Client m_client;
+
+    private class ServerListener implements Session.Listener
     {
-        int SESSIONS = 4;
-        int MESSAGES = 100;
-        int MESSAGE_LENGTH = 250;
+        private Session m_session;
 
-        Test test;
+        public ServerListener( Session session )
+        {
+            m_session = session;
+            System.out.println( "Connection accepted from " + session.getRemoteAddress() );
+        }
 
+        public void onDataReceived( ByteBuffer data )
+        {
+            final int bytesReceived = data.remaining();
+            m_session.sendData( data );
+        }
+
+        public void onConnectionClosed()
+        {
+            System.out.println( "Connection closed to " + m_session.getRemoteAddress() );
+            int sessionsDone = m_sessionsDone.incrementAndGet();
+            if (sessionsDone == m_sessions)
+                m_session.getCollider().stop();
+        }
+    }
+
+    private class TestAcceptor extends Acceptor
+    {
+        public TestAcceptor()
+        {
+            super( new InetSocketAddress(0) );
+            tcpNoDelay = true;
+        }
+
+        public void onAcceptorStarted( int localPort )
+        {
+            System.out.println( "Server started at port " + localPort );
+            m_client = new Client( localPort, m_sessions, m_messages, m_messageLength );
+            m_client.start();
+        }
+
+        public Session.Listener createSessionListener( Session session )
+        {
+            return new ServerListener( session );
+        }
+    }
+
+    private Main( int sessions, int messages, int messageLength )
+    {
+        m_sessions = sessions;
+        m_messages = messages;
+        m_messageLength = messageLength;
+        m_sessionsDone = new AtomicInteger();
+    }
+
+    private void run()
+    {
         try
         {
-            test = new ColliderTest( SESSIONS, MESSAGES, MESSAGE_LENGTH );
-            test.runTest();
+            Collider collider = new Collider();
+            collider.addAcceptor( new TestAcceptor() );
+            collider.run();
+            m_client.stopAndWait();
         }
         catch (IOException ex)
         {
             ex.printStackTrace();
         }
+    }
 
-        test = new NettyTest( SESSIONS, MESSAGES, MESSAGE_LENGTH );
-        test.runTest();
+    public static void main( String args[] )
+    {
+        int sessions = 8;
+        int messages = 1;
+        int messageLength = 250;
+
+        new Main(sessions, messages, messageLength).run();
     }
 }
