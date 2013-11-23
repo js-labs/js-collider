b12eba70ba2a5bac18d41bf991c87a07600f5c23
Sergey Zubarev <info@js-labs.org>
2013-11-24 00:36:31 +0400

    session_throughput test renamed to recv_throughput.

--- tests/src/org/jsl/tests/recv_throughput/Main.java
+++ tests/src/org/jsl/tests/recv_throughput/Main.java
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.jsl.tests.session_throughput;
+package org.jsl.tests.recv_throughput;
 
 public class Main
 {
