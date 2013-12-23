b767e3cc1c901ad920a64491e83147f6bd52bcf8
Sergey Zubarev <info@js-labs.org>
2013-12-23 22:20:11 +0400

    Implemented shared memory IPC test.

---
+++ tests/src/org/jsl/tests/shmem_throughput/Client.java
@@ -0,0 +1,206 @@
+/*
+ * JS-Collider framework.
+ * Copyright (C) 2013 Sergey Zubarev
+ * info@js-labs.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jsl.tests.shmem_throughput;
+
+import org.jsl.collider.Session;
+import org.jsl.collider.ShMemClient;
+import org.jsl.tests.Util;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+
+public class Client
+{
+    public static final int MSG_MAGIC = 0x1ABCDEF2;
+
+    public static class Connector extends org.jsl.collider.Connector
+    {
+        private final boolean m_useShMem;
+        private final ByteBuffer m_msg;
+        private final int m_messages;
+
+        public Connector( InetSocketAddress addr, boolean useShMem, int messages, int messageLength )
+        {
+            super( addr );
+            m_useShMem = useShMem;
+            m_messages = messages;
+
+            /* Server expects messages with the following structure:
+             * 4 bytes : message length
+             * 4 bytes : total messages will be sent
+             * 4 bytes : magic number
+             */
+            assert( messageLength >= 12 );
+            m_msg = ByteBuffer.allocateDirect( messageLength );
+            m_msg.putInt( messageLength );
+            m_msg.putInt( messages );
+            m_msg.putInt( MSG_MAGIC );
+            for (int idx=12; idx<messageLength; idx++)
+                m_msg.put( (byte) idx );
+            m_msg.flip();
+        }
+
+        public Session.Listener createSessionListener( Session session )
+        {
+            return new HandshakeListener( session, m_useShMem, m_messages, m_msg );
+        }
+    }
+
+    private static class HandshakeListener implements Session.Listener
+    {
+        private final Session m_session;
+        private final int m_messages;
+        private final ByteBuffer m_msg;
+        private ShMemClient m_shMem;
+
+        public HandshakeListener( Session session, boolean useShMem, int messages, ByteBuffer msg )
+        {
+            m_session = session;
+            m_messages = messages;
+            m_msg = msg;
+
+            System.out.println( session.getLocalAddress() + ": connected to server " + session.getRemoteAddress() );
+            ByteBuffer buf = null;
+
+            /* Shared memory IPC obviously can be used only for local host connections.
+             * Following approach will not always work properly (for example if SSH tunnel is used),
+             * but fine for the test.
+             */
+            if (useShMem &&
+                (session.getRemoteAddress() instanceof InetSocketAddress) &&
+                (session.getLocalAddress() instanceof InetSocketAddress) &&
+                ((InetSocketAddress) session.getRemoteAddress()).getAddress().isLoopbackAddress())
+            {
+                final InetSocketAddress localAddress = (InetSocketAddress) session.getLocalAddress();
+                try
+                {
+                    ShMemClient shMem = new ShMemClient( Integer.toString(localAddress.getPort()), 64*1024 );
+                    final int descriptorLength = shMem.getDescriptorLength();
+                    buf = ByteBuffer.allocateDirect( 4 + descriptorLength );
+                    buf.putInt( 4 + descriptorLength );
+                    shMem.getDescriptor( buf );
+                    buf.flip();
+                    m_shMem = shMem;
+                    System.out.println( session.getLocalAddress() + ": requesting SHMEM-IPC." );
+                }
+                catch (IOException ex)
+                {
+                    ex.printStackTrace();
+                    useShMem = false;
+                }
+            }
+
+            if (!useShMem)
+            {
+                buf = ByteBuffer.allocateDirect(4);
+                buf.putInt( 0, 4 );
+            }
+
+            session.sendData( buf );
+        }
+
+        public void onDataReceived( ByteBuffer data )
+        {
+            assert( data.remaining() == 4 );
+            if (data.getInt() == 0)
+            {
+                /* Server rejected shared memory IPC */
+                if (m_shMem != null)
+                    m_shMem.close();
+            }
+            else
+            {
+                /* Server accepted shared memory IPC */
+                m_session.accelerate( m_shMem, null );
+            }
+
+            m_session.replaceListener( new Listener(m_session, m_messages, m_msg) );
+        }
+
+        public void onConnectionClosed()
+        {
+            System.out.println( m_session.getLocalAddress() + ": connection closed unexpectedly." );
+            if (m_shMem != null)
+                m_shMem.close();
+        }
+    }
+
+    private static class Sender extends Thread
+    {
+        private final Session m_session;
+        private final int m_messages;
+        private final ByteBuffer m_msg;
+
+        public Sender( Session session, int messages, ByteBuffer msg )
+        {
+            m_session = session;
+            m_msg = msg;
+            m_messages = messages;
+        }
+
+        public void run()
+        {
+            final long startTime = System.nanoTime();
+            for (int idx=0; idx<m_messages; idx++)
+                m_session.sendData( m_msg );
+            final long endTime = System.nanoTime();
+
+            System.out.println(
+                    m_session.getLocalAddress() + ": sent " + m_messages +
+                    " messages (" + m_msg.capacity()*m_messages + " bytes) at " +
+                    Util.formatDelay(startTime, endTime) + " sec." );
+
+            m_session.closeConnection();
+        }
+    }
+
+    private static class Listener implements Session.Listener
+    {
+        private final Session m_session;
+        private final Sender m_sender;
+
+        public Listener( Session session, int messages, ByteBuffer msg )
+        {
+            m_session = session;
+            m_sender = new Sender( session, messages, msg );
+            m_sender.start();
+        }
+
+        public void onDataReceived( ByteBuffer data )
+        {
+            /* Should never happen by test design. */
+            assert( false );
+        }
+
+        public void onConnectionClosed()
+        {
+            try
+            {
+                m_sender.join();
+            }
+            catch (InterruptedException ex)
+            {
+                ex.printStackTrace();
+            }
+            System.out.println( m_session.getLocalAddress() + ": connection closed." );
+        }
+    }
+}
---
+++ tests/src/org/jsl/tests/shmem_throughput/Server.java
@@ -0,0 +1,172 @@
+/*
+ * JS-Collider framework.
+ * Copyright (C) 2013 Sergey Zubarev
+ * info@js-labs.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jsl.tests.shmem_throughput;
+
+import org.jsl.collider.Collider;
+import org.jsl.collider.Session;
+import org.jsl.collider.ShMemServer;
+import org.jsl.collider.StreamDefragger;
+import org.jsl.tests.Util;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Server
+{
+    public static class Acceptor extends org.jsl.collider.Acceptor
+    {
+        private final int m_sessions;
+        private final int m_messages;
+        private final int m_messageLength;
+        private final AtomicInteger m_sessionsDone;
+
+        public Acceptor( int sessions, int messages, int messageLength )
+        {
+            m_sessions = sessions;
+            m_messages = messages;
+            m_messageLength = messageLength;
+            m_sessionsDone = new AtomicInteger( sessions );
+        }
+
+        public Session.Listener createSessionListener( Session session )
+        {
+            return new HandshakeListener( session, m_sessionsDone );
+        }
+
+        public void onAcceptorStarted( Collider collider, int portNumber )
+        {
+            System.out.println( "Server started at port " + portNumber );
+            final InetSocketAddress addr = new InetSocketAddress( InetAddress.getLoopbackAddress(), portNumber );
+            for (int idx=0; idx<m_sessions; idx++)
+                collider.addConnector( new Client.Connector(addr, true, m_messages, m_messageLength) );
+        }
+    }
+
+    private static class HandshakeListener implements Session.Listener
+    {
+        private final Session m_session;
+        private final AtomicInteger m_sessionsDone;
+
+        public HandshakeListener( Session session, AtomicInteger sessionsDone )
+        {
+            m_session = session;
+            m_sessionsDone = sessionsDone;
+            System.out.println( session.getRemoteAddress() + ": connection accepted." );
+        }
+
+        public void onDataReceived( ByteBuffer data )
+        {
+            final int bytesReceived = data.remaining();
+            final int messageLength = data.getInt();
+            if (bytesReceived != messageLength)
+            {
+                System.out.println( m_session.getRemoteAddress() + ": invalid message received." );
+                m_session.getCollider().stop();
+            }
+
+            ShMemServer shMem = null;
+            if (messageLength > 4)
+            {
+                try { shMem = new ShMemServer( data ); }
+                catch (Exception ex) { ex.printStackTrace(); }
+            }
+
+            ByteBuffer reply = ByteBuffer.allocateDirect(4);
+            if (shMem == null)
+            {
+                reply.putInt( 0, 0 );
+                m_session.sendData( reply );
+            }
+            else
+            {
+                reply.putInt( 0, 1 );
+                m_session.accelerate( shMem, reply );
+            }
+
+            m_session.replaceListener( new Listener(m_session, m_sessionsDone) );
+        }
+
+        public void onConnectionClosed()
+        {
+            System.out.println( m_session.getRemoteAddress() + ": connection closed unexpectedly." );
+        }
+    }
+
+    private static class Listener implements Session.Listener
+    {
+        private final Session m_session;
+        private final AtomicInteger m_sessionsDone;
+        private final StreamDefragger m_stream;
+        private int m_messagesReceived;
+        private long m_startTime;
+
+        public Listener( Session session, AtomicInteger sessionsDone )
+        {
+            m_session = session;
+            m_sessionsDone = sessionsDone;
+
+            m_stream = new StreamDefragger(4)
+            {
+                protected int validateHeader( ByteBuffer header )
+                {
+                    assert( header.remaining() >= 4 );
+                    return header.getInt();
+                }
+            };
+        }
+
+        public void onDataReceived( ByteBuffer data )
+        {
+            ByteBuffer msg = m_stream.getNext( data );
+            while (msg != null)
+            {
+                final int bytesReady = msg.remaining();
+                final int messageLength = msg.getInt();
+                final int totalMessages = msg.getInt();
+                assert( bytesReady == messageLength );
+                assert( msg.getInt() == Client.MSG_MAGIC );
+                final int messagesReceived = ++m_messagesReceived;
+                if (messagesReceived == 1)
+                    m_startTime = System.nanoTime();
+                else if (messagesReceived == totalMessages)
+                {
+                    long endTime = System.nanoTime();
+                    System.out.println(
+                            m_session.getRemoteAddress() + ": received " + totalMessages +
+                            " messages (" + messageLength * totalMessages + " bytes) at " +
+                            Util.formatDelay(m_startTime, endTime) + " sec." );
+                    m_session.closeConnection();
+                }
+                msg = m_stream.getNext();
+            }
+        }
+
+        public void onConnectionClosed()
+        {
+            System.out.println( m_session.getRemoteAddress() + ": connection closed." );
+            int sessionsDone = m_sessionsDone.decrementAndGet();
+            assert( sessionsDone >= 0 );
+            if (sessionsDone == 0)
+                m_session.getCollider().stop();
+        }
+    }
+}
